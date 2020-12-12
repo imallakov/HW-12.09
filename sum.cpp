8d4841421b6dea96392ad26bd10822a3792d0a1b
Yakup <72393805+yakupchik@users.noreply.github.com>
2020-12-12 13:51:21 +0500

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main()
 {
 	int n,x,m=0;
-	cin>>n>>x;
+	cout<<"Input x:";
+	cin>>x;
+	cout<<"Input n:";
+	cin>>n;
 	double S=0,eps=1e-6;
 	int k=0;
+	int a=0;
 	do
 	{
 		double l=1,m=1;
@@ -17,9 +21,11 @@ int main()
 		}
 		double sum=l/m;
 		if(k%2!=0) sum*=-1;
-		std::cout<<"k="<<k<<"   "<<x<<"^"<<2*k+1<<"="<<l<<"   "<<2*k+1<<"!="<<m<<"     S="<<S<<"    sum="<<sum<<std::endl;
+//		std::cout<<"k="<<k<<"   "<<x<<"^"<<2*k+1<<"="<<l<<"   "<<2*k+1<<"!="<<m<<"     S="<<S<<"    sum="<<sum<<std::endl;
 		S+=sum;
 		k++;
+		a++;
 	}while(k<=n && (S>=eps || S<=-eps));
-	cout<<S;
+	cout<<"Sum = "<<S<<endl;
+	cout<<"Number of addends = "<<a<<endl;
 }
